3d1826fbffb5c6acf26413c5143228cd5d1db7a8
tip52 <103219982+tip52@users.noreply.github.com>
2022-11-03 16:23:14 -0600

    Update Get Ancestor Amount.lua

--- Get Ancestor Amount.lua
+++ Get Ancestor Amount.lua
@@ -1,16 +1,1 @@
---This function will return the amount of ancestors an item has, pretty easy to make since I changed 1 variable.
 
-local function GetAncestorAmt(v)
-    local ancestors = {}
-        local ancestor = v.Parent
-        repeat
-            task.wait()
-            ancestor = ancestor.Parent
-            table.insert(ancestors,ancestor)
-
-        until ancestor.Parent == nil;
-
-    end
-
---Usage
-print(GetAncestorAmt(game.Players.LocalPlayer.Character))
