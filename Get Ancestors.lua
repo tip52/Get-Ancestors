034937ee2002df8a2171145b13caaa98715e1e1b
tip52 <103219982+tip52@users.noreply.github.com>
2022-11-03 16:31:33 -0600

    Update Get Ancestors.lua

--- Get Ancestors.lua
+++ Get Ancestors.lua
@@ -1,3 +1,5 @@
+-- a function  to get the ancestors (parents) of existing parts inside of the workspace.
+
 local function GetAncestors(v)
     local ancestors = {}
     local ancestor = v.Parent
@@ -5,14 +7,12 @@ local function GetAncestors(v)
         task.wait()
         ancestor = ancestor.Parent
         table.insert(ancestors,ancestor)
-
-
     until ancestor.Parent == nil;
-
-
+    table.insert(ancestors,#ancestors)
     return ancestors
 end
 
-
-print(table.unpack(GetAncestors(game:GetService("Workspace")["out of bounds"].tree.MeshPart)))
-
+--Usage
+local AncestorTable = GetAncestors(game.Players.LocalPlayer.Character) -- setting table variable
+print(table.unpack(AncestorTable)) -- Unpacking the table
+print(AncestorTable[1]) -- printing the first item in the ancestor table
