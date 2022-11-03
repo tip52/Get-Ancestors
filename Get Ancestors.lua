86a6ac3b3421d7eb14147094c23c66f7bb548070
tip52 <103219982+tip52@users.noreply.github.com>
2022-11-03 16:26:21 -0600

    Update Get Ancestors.lua

--- Get Ancestors.lua
+++ Get Ancestors.lua
@@ -1,19 +1,18 @@
---The GetAncestors function requires an item to get the ancestors of, it will return a table.
---Please feel free to make this better, I did not spend much time creating it.
-
 local function GetAncestors(v)
     local ancestors = {}
-        local ancestor = v.Parent
-        repeat
-            task.wait()
-            ancestor = ancestor.Parent
-                table.insert(ancestors,ancestor)
+    local ancestor = v.Parent
+    repeat
+        task.wait()
+        ancestor = ancestor.Parent
+        table.insert(ancestors,ancestor)
 
-        until ancestor.Parent == nil;
 
-            return ancestors, #ancestors
-    end
+    until ancestor.Parent == nil;
 
---You can set a variable to the function like this
-local returned_table = GetAncestors(game.Players.LocalPlayer.Character)
-print(table.unpack(returned_table)) -- we use table.unpack as the function does not do it for us (for various reasons such as not being able to reference items inside the table.)
+
+    return ancestors
+end
+
+
+print(table.unpack(GetAncestors(game:GetService("Workspace")["out of bounds"].tree.MeshPart)))
+
