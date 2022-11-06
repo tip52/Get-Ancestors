6c7fe693d05d19ad1908f5dfd4d8c5c896a6e2dd
tip52 <103219982+tip52@users.noreply.github.com>
2022-11-06 10:56:04 -0700

    Update Get Ancestors.lua

--- Get Ancestors.lua
+++ Get Ancestors.lua
@@ -4,7 +4,6 @@ local function GetAncestors(v)
     local ancestors = {}
     local ancestor = v.Parent
     repeat
-        task.wait()
         ancestor = ancestor.Parent
         table.insert(ancestors,ancestor)
     until ancestor.Parent == nil;
